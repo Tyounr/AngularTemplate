9853a4c22b6fffad91fc683fe566244e47813683
Ruben Szekér <rubenszeker@outlook.com>
2021-08-05 12:24:57 +0200

    Added app-shell

--- Angular-project-template/src/app/app.module.ts
+++ Angular-project-template/src/app/app.module.ts
@@ -1,33 +1,37 @@
-import { NgModule } from '@angular/core';
-import { environment } from '@env/environment';
+import { NgModule } from "@angular/core";
+import { environment } from "@env/environment";
 
 // Routing
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
 // App component
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 // PWA
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { ServiceWorkerModule } from "@angular/service-worker";
 
 // Browser
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 // Shared module
-import { SharedModule } from '@shared/shared.module';
+import { SharedModule } from "@shared/shared.module";
+import { RouterModule } from "@angular/router";
 
 // Animations
 
 @NgModule({
-  declarations: [ AppComponent ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }), // Necessary to run the application,
+    BrowserModule.withServerTransition({ appId: "serverApp" }), // Necessary to run the application,
     BrowserAnimationsModule, // To support animations
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }), // Necessary for PWA
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production,
+    }), // Necessary for PWA
     SharedModule, // Shared modules
     AppRoutingModule,
+    RouterModule,
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Angular-project-template/src/app/app.server.module.ts
+++ Angular-project-template/src/app/app.server.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { NgModule } from "@angular/core";
+import { ServerModule } from "@angular/platform-server";
 
-import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppModule } from "./app.module";
+import { AppComponent } from "./app.component";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { Routes, RouterModule } from "@angular/router";
+import { AppShellComponent } from "./app-shell/app-shell.component";
+
+const routes: Routes = [{ path: "shell", component: AppShellComponent }];
 
 @NgModule({
+  declarations: [AppShellComponent],
   imports: [
     AppModule,
     ServerModule,
     FlexLayoutModule, // To use flex layout with SSR
+    RouterModule.forRoot(routes),
   ],
   bootstrap: [AppComponent],
 })
